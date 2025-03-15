1fc0b8ec85b1c23b6315130aff80e1326d6a0401
codersunny812 <sengersunny448@gmail.com>
2025-03-16 03:04:25 +0530

    bug fixing

--- server/app.js
+++ server/app.js
@@ -12,12 +12,9 @@ connectDb();
 
 
 const PORT = process.env.PORT || 4001;
-const uri = process.env.FRONTEND_URI;
 //middle ware
 app.use(express.json())
-app.use(cors({
-    origin:uri
-}))
+app.use(cors({}))
 
 
 
